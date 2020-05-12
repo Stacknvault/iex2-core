fe066ca36e07a4b3687ff8d4bd39c82c40d303e9
Andoni Arostegui <andoni.arostegui@stacknvault.com>
2020-05-12 11:59:54 +0200

    New image carousel

--- src/index.jsx
+++ src/index.jsx
@@ -4,5 +4,6 @@ export { default as LogoBanner } from './sections/LogoBanner';
 export { default as ProvisionContractAgreement } from './sections/ProvisionContractAgreement';
 export { default as SimpleDataTable }  from './sections/SimpleDataTable';
 export { default as SimpleHeroBanner } from './sections/SimpleHeroBanner';
+export { default as ImageCarousel } from './sections/ImageCarousel/ImageCarousel';
 export { default as Stage } from './Stage';
 export { ContextStore as ContextStore } from './Stage';
