da813f5e5dec7480a584952c2d26a19a7ee457d3
Katia Sheleh <kate.sheleh@gmail.com>
2021-05-17 14:26:42 +0200

    update context

--- src/lib/Context.js
+++ src/lib/Context.js
@@ -1,16 +1,18 @@
-import React, {Fragment, useContext, useEffect, useState} from 'react'
+import React, {useContext, useEffect, useState} from 'react'
 import jp from 'jsonpath'
+import mustache from 'mustache'
+
+export const supportedLanguages = ['en', 'es', 'de', 'it', 'fr' ];
 
 const appStage = process.env.REACT_APP_STAGE || 'production'
 const devProd = appStage === 'production' || appStage === 'staging' ? 'prod' : 'dev'
-const getDevProd = () => {
+export const getDevProd = () => {
   return devProd
 }
-const getFFUrl = () => {
+export const getFFUrl = () => {
   return `https://api.${appStage}.cloudios.flowfact-${devProd}.cloud`
-
 }
-const flattenEntity = (entity) => {
+export const flattenEntity = (entity) => {
   let entityCopy = {}
   entity && Object.keys(entity).map(key => {
     if (entity[key] && entity[key].values) {
@@ -18,11 +20,15 @@ const flattenEntity = (entity) => {
         entityCopy[key] = entity[key].values[0]
       }
     }
+    if (entity[key] && !entity[key].values) {
+      entityCopy[key] = entity[key]
+    }
   })
-  entityCopy._raw = { ...entityCopy }
+  entityCopy._raw = {...entityCopy}
   return entityCopy
 }
-const getExternalConfig = () => {
+
+export const getExternalConfig = () => {
   if (window.document.location.hash) {
     try {
       return JSON.parse(decodeURI(window.document.location.hash.substring(1)))
@@ -34,25 +40,66 @@ const getExternalConfig = () => {
   }
   return undefined
 }
+
+export const getExposeId = () => {
+  const m = document.location.href.match('\/render\/([^\/]*)')
+  const local = !m || m.length < 2
+  if (local) {
+    return null;
+  } else {
+    const exposeId = m[1] // maybe better it came from the context
+    return exposeId;
+  }
+}
+export const getBackendUrl = () => {
+  return `https://iex2-expose-lambda.${appStage}.sf.flowfact-${devProd}.cloud`;
+}
 const externalConfig = getExternalConfig()
 
 const ContextStore = React.createContext({})
 const Context = ({children}) => {
   const [iex, setIEX] = useState({})
-  const [customConfig, setCustomConfig] = useState({})
   const [ready, setReady] = useState(false)
   const [error, setError] = useState(null)
   const [config, setConfig] = useState(false)
   const [contractAcceptances, setContractAcceptances] = useState({})
+  const [hostname, setHostname] = useState('')
+  const [language, setLanguage] = useState('de')
 
-  function upgradeStage(toStage) {
+  const filterContracts = (contracts) => {
+    let filteredContracts = (contracts && contracts
+      .filter((item) => {
+        //console.log('item.technicalName', item.technicalName);
+        return item.technicalName.endsWith(`-${language}`)
+      })
+      .sort((item) => (!item.technicalName.startsWith('terms_of_use') ? 1 : -1))) || []
+    if (filteredContracts.length === 0){
+      if (contracts.length > 0){
+        const m = contracts[0].technicalName.match('-(.*)$');
+        if (m && m.length>1){
+          const firstLang = m[1];
+          //console.log('firstLang', firstLang);
+          filteredContracts = contracts.filter((contract) => contract.technicalName.endsWith(`-${firstLang}`))
+          .sort((item) => (!item.technicalName.startsWith('terms_of_use') ? 1 : -1));
+        }else {
+          filteredContracts = contracts;
+        }
+      } else {
+        filteredContracts = [];
+      }
+    }
+    // filter if there is no commission
+    return filteredContracts.filter((contract) => (!contract.technicalName.startsWith('commission_hint') && !contract.technicalName.startsWith('revocation_terms')) || ffmap`estate.commissionProspect`);
+  }
+
+  function upgradeStage(toStage, vars) {
     // console.log('contractAcceptances', contractAcceptances);
     const m = document.location.href.match('\/render\/([^\/]*)')
     const local = !m || m.length < 2
     if (local) {
       const newIex = {...iex}
       newIex.stage++
-      setIEX(newIex)
+      setIEX(newIex);
     } else {
       const exposeId = m[1] // maybe better it came from the context
       const dateStr = new Intl.DateTimeFormat('de-DE', {
@@ -65,28 +112,33 @@ const Context = ({children}) => {
 
       var message = `<p>${dateStr} Uhr: Der Besucher hat das interaktive Exposé besucht</p>
 
-              <p>14.Sep. 09:57 Uhr: Es wurden mehr Informationen angefordert</p>`
+              <p>${dateStr} Uhr: Es wurden mehr Informationen angefordert</p>`
 
       var acceptedContracts = []
 
       Object.keys(contractAcceptances).length && iex.company.legislationTexts.map(item => {
-        var legislationCheckboxes = item.legislationCheckboxes.filter(i => contractAcceptances[`${item.id}-${i.value}`])
+        var legislationCheckboxes = item.legislationCheckboxes.filter(i => contractAcceptances[`${item.id}-${i.value}`]).map(cb => ({
+          ...cb,
+          value: mustache.render(cb.value, vars),
+          label: mustache.render(cb.label, vars),
+        }))
+        item.legislationTextContent = mustache.render(item.legislationTextContent, vars)
+
         var contract = {...item, legislationCheckboxes}
         acceptedContracts = [...acceptedContracts, contract]
         legislationCheckboxes.map(res => {
-          message += `<p>✓${res.label}<\p>
-                    `
+          message += `<p>✓${res.label}<\p>`
         })
       })
       message += `<p>${dateStr} Uhr: Der Besucher hat das interaktive Exposé verlassen<\p>\n`
-      //   console.log('message', message);
 
-      fetch(`https://iex2-expose-lambda.${appStage}.sf.flowfact-${devProd}.cloud/public/expose/${exposeId}/track?async`,
+
+      fetch(`${getBackendUrl()}/public/expose/${exposeId}/track?async`,
         {
           headers: {'content-type': 'application/json'},
           body: JSON.stringify({
             subject: 'Neuer Besucher im Interaktiven Exposé',
-            message: message,
+            message: mustache.render(message, vars),
             stage: toStage,
             acceptedContracts,
           }),
@@ -98,7 +150,6 @@ const Context = ({children}) => {
           const newIex = {...iex}
           newIex.stage++
           setIEX(newIex)
-          setCustomConfig(newIex.config||{})
           fetchContext(true, newIex.stage)
           // for some reason this is not always working
           // setTimeout(()=>window.document.location.reload(), 1000)
@@ -112,60 +163,100 @@ const Context = ({children}) => {
     setContractAcceptances(ca)
   }
 
+  
+
+  const applyMappings = (iex, mappings) => {
+    if (mappings) {
+      Object.keys(mappings).map((mappingName) => {
+        const schemaIndex = iex.schemas.findIndex((schema) => schema.name === iex[mappingName]._metadata.schema);
+        const mappingArray = mappings[mappingName];
+        mappingArray.map((mapping) => {
+          ['', ...supportedLanguages].map((language) => {
+            const LANG = language.toUpperCase();
+            const goodKeyIndex = mapping.findIndex((item) => {
+              const key = `${item}${LANG}`;
+              const value = iex[mappingName][key];
+              return value;
+            });
+            if (goodKeyIndex >= 0) {
+              for (let i = 0; i<mapping.length; i++) {
+                if(i!==goodKeyIndex) {
+                  const localizedMapping = `${mapping[i]}${LANG}`;
+                  const localizedKey = `${mapping[goodKeyIndex]}${LANG}`;
+                  iex[mappingName][localizedMapping]=iex[mappingName][localizedKey];
+                  if (schemaIndex >= 0 && !iex.schemas[schemaIndex].properties[localizedMapping]) {
+                    // we need to add the property to the schema too
+                    iex.schemas[schemaIndex].properties[localizedMapping] = iex.schemas[schemaIndex].properties[mapping[goodKeyIndex]];
+                  }
+                }
+              }
+            }
+          });
+        });
+      });
+    }
+    return iex;
+  }
+
   const jsonify = res => res.json()
   const reportError = error => {
     setError('' + error)
   }
   const fetchContext = (expectStageChange, targetStage) => {
-    console.log('fetching context again')
     var contextURL = externalConfig && externalConfig.contextURL ? externalConfig.contextURL : `assets/context/context.json?${Math.random()}`
-    fetch(contextURL, {headers: {cognitotoken: externalConfig && externalConfig.cognitotoken}})
+    const headers = externalConfig ? {...externalConfig.authHeaders} : {}
+    fetch(contextURL, {headers: headers})
       .then(jsonify)
-      .then(result => {
-        if (expectStageChange && targetStage !== result.stage) {
+      .then(iex => {
+        if (expectStageChange && targetStage !== iex.stage) {
           // let's try it again
           // console.log('Retrying context fetch until we see a stage change')
           setTimeout(() => fetchContext(true, targetStage), 1000)
           return
         }
-        setCustomConfig(result.config||{})
-        setIEX(result)
         setError(null)
-        fetch(`assets/context/config.json?${Math.random()}`, {headers: {cognitotoken: externalConfig && externalConfig.cognitotoken}})
+        fetch(`assets/context/config.json?${Math.random()}`, {headers: headers})
           .then(jsonify)
-          .then(result => {
+          .then(config => {
             // resetMissingVars();
-            setConfig(result)
+            setConfig(config)
             setReady(true)
+            // we apply the field mappings here
+            const mappings = config.fieldMapping;
+            setIEX(applyMappings(iex, mappings));
           }, reportError)
       }, reportError)
+      .catch(error => console.log(error))
   }
 
   useEffect(fetchContext, [])
   if (!ready) {
-    return <Fragment/>
+    return <></>
   }
   const {stage} = iex
-  
+
   return <ContextStore.Provider value={{
     iex,
-    customConfig,
-    setCustomConfig,
     config,
     ready,
     error,
     upgradeStage,
     stage,
     setContractAccepted,
+    hostname,
+    setHostname,
+    language,
+    setLanguage,
+    filterContracts,
   }}>{children}</ContextStore.Provider>
 }
 
 const Stage = ({level, children}) => {
   const {iex, ready} = useContext(ContextStore)
   const {stage} = iex
-  if (!ready) return <Fragment/>
-  if ('' + stage !== level) return <Fragment/>
-  return <Fragment>{children}</Fragment>
+  if (!ready) return <></>
+  if ('' + stage !== level) return <></>
+  return <>{children}</>
 }
 
 const resetMissingVars = () => {
@@ -178,9 +269,9 @@ const addMissingVar = (ms) => {
     window.missingVars.sort()
   }
 }
-const ffmap = (strings, ...values) => {
-  var str = ''
-  for (var i = 0; i < strings.length; i++) {
+const __ffmap = (strings, ...values) => {
+  let str = ''
+  for (let i = 0; i < strings.length; i++) {
     str += strings[i]
     if (i < values.length) {
       str += values[i]
@@ -200,9 +291,12 @@ const ffmap = (strings, ...values) => {
     let answer = q(`$.${str}.values`)
     // console.log("Answer with .values: ",answer)
     answer = answer[0]
+    if (Array.isArray(answer) && answer.length > 1) {
+      return answer
+    }
 
     // undue the weird everything is a value array in FF
-    if (Array.isArray(answer) && answer.length === 1) {
+    if (Array.isArray(answer) && answer.length) {
       // console.log("Unwrapping Array : ",answer[0]);
       answer = answer[0]
     }
@@ -246,4 +340,21 @@ const ffmap = (strings, ...values) => {
 }
 
 
-export {Context, ContextStore, Stage, ffmap, resetMissingVars, getDevProd, getFFUrl, flattenEntity, getExternalConfig}
+const ffmap = (strings, ...values) => {
+  const {language} = ContextStore._currentValue
+  let value = __ffmap(strings, ...values)
+  if (!value && language) {
+    const lng = language.substring(0, 2).toUpperCase()
+    // const newKey = strings[0].replace(/([^\.]+\.[^\.]+)(.?.*)$/, '$1EN$2');
+    const newKey = strings[0]
+    value = __ffmap(`${newKey}${lng}`, ...values)
+    if (!value && lng === 'DE'){
+      // try also with 'ch'
+      value = __ffmap(`${newKey}CH`, ...values)
+    }
+  }
+  return value
+}
+
+
+export {Context, ContextStore, Stage, ffmap, resetMissingVars}
